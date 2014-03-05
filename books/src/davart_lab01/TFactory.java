67f02487e436034b46bec87d6443b2a188161fbd
David <davisonchu25@gmail.com>
2014-03-05 21:14:33 +0100

    38 slide implemented

--- books/src/davart_lab01/TFactory.java
+++ books/src/davart_lab01/TFactory.java
@@ -6,6 +6,8 @@ package davart_lab01;
 
 import java.util.Date;
 
+import javax.management.RuntimeErrorException;
+
 /**
  *
  * @author UML05
@@ -17,8 +19,38 @@ public class TFactory {
         return null;
     }
 
-    public TTitle_book create_title_book(String[] data) {
-        return null;
+    public TTitle_book create_title_book(String[] data) 
+    {
+    	TTitle_book title_book;
+    	switch(Integer.parseInt(data[0]))
+    	{
+	    	case 0:	
+	    		title_book = new TTitle_book();
+	    		title_book.setISBN(data[1]);
+	    		return title_book;
+	    	case 1:
+	    		title_book = new TTitle_book();
+	    		title_book.setAuthor(data[1]);
+	    		title_book.setTitle(data[2]);
+	    		title_book.setISBN(data[3]);
+	    		title_book.setPublisher(data[4]);
+	    		return title_book;
+	    	case 2:
+	    		TTitle_book_on_tape title_book1 = new TTitle_book_on_tape();
+	    		title_book1.setISBN(data[1]);
+	    		title_book1.setActor(data[2]);
+	    		return title_book1;
+	    	case 3:
+	    		TTitle_book_on_tape title_book2 = new TTitle_book_on_tape();
+	    		title_book2.setAuthor(data[1]);
+	    		title_book2.setTitle(data[2]);
+	    		title_book2.setISBN(data[3]);
+	    		title_book2.setPublisher(data[4]);
+	    		title_book2.setActor(data[5]);
+	    		return title_book2;
+			default:
+				throw new RuntimeException("unknown method for creating title book");
+    	}
     }
 
     public TBook create_book(String[] data) {
