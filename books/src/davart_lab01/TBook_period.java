c2556a1dbd714a7dd495e2320edbf9bcf46aefa0
Artur Z <arjamizo@gmail.com>
2014-03-06 16:59:50 +0100

    added TBook_period

---
+++ books/src/davart_lab01/TBook_period.java
@@ -0,0 +1,16 @@
+package davart_lab01;
+
+import java.io.Serializable; 
+import java.util.Date;
+
+public class TBook_period extends TBook implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Date period;
+	public Date getPeriod () { return null; }
+	public void setPeriod (Date date) { }
+	public String toString () { return null; }
+	public boolean period_pass(Object data)
+	{ return true; }
+	public void startPeriod(Object data)
+	{ }
+}
