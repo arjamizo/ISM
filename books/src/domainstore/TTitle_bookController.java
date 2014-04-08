8cf755e0ccd7e57b2335eb54972d63e9b38d0da7
Artur Z <arjamizo@gmail.com>
2014-04-09 01:03:20 +0200

    pages 31-35

--- books/src/domainstore/TTitle_bookController.java
+++ books/src/domainstore/TTitle_bookController.java
@@ -6,7 +6,10 @@
 
 package domainstore;
 
+import java.util.Iterator;
+import java.util.List;
 import javax.persistence.*;
+import sub_business_tier.entities.TTitle_book;
 
 /**
  *
@@ -21,5 +24,48 @@ public class TTitle_bookController {
             emf = Persistence.createEntityManagerFactory("booksPU");
         }
         return emf.createEntityManager();
+    }
+    public boolean addTTitle_book(TTitle_book title_book) {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(title_book);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+            return false;
+        }
+    }
+    public boolean addTTitle_books(List<TTitle_book> titles) {
+        EntityManager em = getEntityManager();
+        TTitle_book newTTitle_book = null;
+        try {
+            Iterator it = titles.iterator();
+            em.getTransaction().begin();
+            while (it.hasNext()) {
+                newTTitle_book = (TTitle_book) it.next();
+                if (newTTitle_book.getId() == null) {
+                    em.persist(newTTitle_book);
+                }
+            }
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+            return false;
+        }
+    }
+    public TTitle_book[] getTTitle_books_() {
+        return (TTitle_book[]) getTTitle_books().toArray(new TTitle_book[0]);
+    }
+
+    public List<TTitle_book> getTTitle_books() {
+        EntityManager em = getEntityManager();
+        try {
+            javax.persistence.Query q
+                    = em.createQuery("select c from TTitle_book as c");
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
     }
 }
