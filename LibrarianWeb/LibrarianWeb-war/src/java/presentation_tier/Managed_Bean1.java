1abae29ad218ee17369968fd103915eac68e4d5a
Artur Z <arjamizo@gmail.com>
2014-04-15 02:40:25 +0200

    creatd managed bean; added button to store data; fixed layout and links; added table to show data

---
+++ LibrarianWeb/LibrarianWeb-war/src/java/presentation_tier/Managed_Bean1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package presentation_tier;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author azochniak
+ */
+@ManagedBean
+@RequestScoped
+public class Managed_Bean1 {
+
+    /**
+     * Creates a new instance of Managed_Bean1
+     */
+    public Managed_Bean1() {
+    }
+    
+}
